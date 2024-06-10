01afde8d94e601ec853da73eb456d62d07d3b1de
M-M394 <mmitchell36@mail.pima.edu>
2024-06-10 15:46:47 -0700

    Update cis129_ lab02_shortStory.py
    this is a short story about a bunny.

--- cis129_ lab02_shortStory.py
+++ cis129_ lab02_shortStory.py
@@ -1,2 +1,7 @@
-print("Hello, World!")
+#Author: Mikayla Mitchell
+#CIS129 python lab 2
+#printing a short story
+print("There was once a tiny rabbit in the garden.")
+print("It loved to feast on the fruits that resided in the greta garden. They found the strawberries to be quite delicious.")
+print("Then one day the bunny decided to move on to another garden. This garden was filled with delicious vegtables and the bunny new that they would live here happily.")
       
